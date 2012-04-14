605eada320406ff59e0eda8da805fa9f403ecc1e
Bob McWhirter <bob@mcwhirter.org>
2012-04-14 16:35:26 -0400

    Add extensions and transformers to the pipeline.

--- lib/awestruct/pipeline.rb
+++ lib/awestruct/pipeline.rb
@@ -6,9 +6,13 @@ module Awestruct
   class Pipeline
 
     attr_reader :handler_chains
+    attr_reader :extensions
+    attr_reader :transformers
 
     def initialize()
       @handler_chains = HandlerChains.new
+      @extensions     = []
+      @transformers   = []
     end
 
   end
